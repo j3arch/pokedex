85776684c4eb6b14b48893962b7770ffc14750a1
j3arch <j3archx@gmail.com>
2026-04-27 22:26:42 +0300

    repl

--- repl.go
+++ repl.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	// "bufio"
-	// "fmt"
-	// "os"
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func startRepl() {
-	// setup repl
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
 }
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
-}
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func getCommands() map[string]cliCommand {
-	return nil
 }
