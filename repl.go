abd3622caae072a56948bcb9b9f9750df6712729
j3arch <j3archx@gmail.com>
2026-07-28 21:36:40 +0300

    get commands

--- repl.go
+++ repl.go
@@ -45,3 +45,18 @@ type cliCommand struct {
 	description string
 	callback    func() error
 }
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
