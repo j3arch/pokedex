cfe23acb648598c95e046c340de7bc8734f71180
j3arch <j3archx@gmail.com>
2026-08-01 19:35:56 +0300

    client fix

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -2,17 +2,14 @@ package pokeapi
 
 import (
 	"net/http"
-	"time"
 )
 
 type Client struct {
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient() Client {
 	return Client{
-		httpClient: http.Client{
-			Timeout: timeout,
-		},
+		httpClient: http.Client{},
 	}
 }
