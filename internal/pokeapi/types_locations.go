e9b5c252b7b0a60e598aaa9b3673de69ddecb912
j3arch <j3archx@gmail.com>
2026-08-01 19:24:42 +0300

    json struct fix

--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -1,8 +1,11 @@
 package pokeapi
 
 type RespShallowLocations struct {
-	Count    int      `json:"count"`
-	Next     *string  `json:"next"`
-	Previous *string  `json:"previous"`
-	Results  []result `json:"results"`
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"result"`
 }
