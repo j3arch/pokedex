49d98ca8165e4f9f5daff4f9dea522d3b9ab8455
j3arch <j3archx@gmail.com>
2026-08-05 21:14:16 +0300

    map forward command

--- command_map.go
+++ command_map.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 func commandMapf(cfg *config) error {
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.prevLocationsURL = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
 
