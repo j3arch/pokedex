d16e25e651f8ea2065b030cf9b49f2935ce3b93e
j3arch <j3archx@gmail.com>
2026-08-05 21:25:57 +0300

    map back command

--- command_map.go
+++ command_map.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -20,5 +21,20 @@ func commandMapf(cfg *config) error {
 }
 
 func commandMapb(cfg *config) error {
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you're on the first page")
+	}
+
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.nextLocationsURL = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
