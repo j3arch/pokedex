36bb3cc15206f6be4d90288041922e648cf3d76f
j3arch <j3archx@gmail.com>
2026-08-08 20:26:03 +0300

    base cases

--- internal/pokecache/pockecache_test.go
+++ internal/pokecache/pockecache_test.go
@@ -1,1 +1,24 @@
 package pokecache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAddGet(t *testing.T) {
+	const interval = 5 * time.Second
+	cases := struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "https://example.com",
+			val: []byte("testdata"),
+		},
+		{
+			key: "https://example.com/path",
+			val: []byte("moretestdata"),
+		},
+	}
+
+}
