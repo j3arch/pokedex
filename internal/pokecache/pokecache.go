8cde88129fc9b5cf7adbfbe2446a5fac48887873
j3arch <j3archx@gmail.com>
2026-08-08 20:30:26 +0300

    cache and new cache structs

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,5 +1,16 @@
 package pokecache
 
+import (
+	"sync"
+	"time"
+)
+
 type Cache struct {
-	cache map[string]string
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
+type CacheEntry struct {
+	createdAt time.Time
+	val       []byte
 }
