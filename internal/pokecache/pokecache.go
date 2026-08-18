e6d47b343b7febf17fe4074e6042aafe75a187f6
j3arch <j3archx@gmail.com>
2026-08-18 23:05:39 +0300

    reap loop

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -51,5 +51,12 @@ func (c *Cache) reapLoop(interval time.Duration) {
 }
 
 func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
 
 }
