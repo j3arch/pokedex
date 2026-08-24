41833271b0d92dbef42fd3ebfc702398d2217906
j3arch <j3archx@gmail.com>
2026-08-24 23:10:47 +0300

    data package

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,7 +1,20 @@
 package pokeapi
 
+import (
+	"encoding/json"
+)
+
 func (c *Client) GetLocation(locationName string) (Location, error) {
-	locationResp := Location{}
+	url := baseURL + "/location-area/" + locationName
+
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := Location{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return Location{}, err
+		}
+		return locationResp, nil
+	}
 
 	return locationResp, nil
 }
