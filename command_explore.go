34632bd4fbf0feab66fee2a468e30ea0afa9141c
j3arch <j3archx@gmail.com>
2026-08-24 22:59:37 +0300

    error handling

--- command_explore.go
+++ command_explore.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"errors"
+)
+
 func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("You must provide a location name")
+	}
 	return nil
 }
