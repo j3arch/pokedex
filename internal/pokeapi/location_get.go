75f55839710574be515c0874d2c6e0127a77d01b
j3arch <j3archx@gmail.com>
2026-08-24 23:14:17 +0300

    fix

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -22,6 +22,11 @@ func (c *Client) GetLocation(locationName string) (Location, error) {
 	if err != nil {
 		return Location{}, err
 	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Location{}, err
+	}
 	defer resp.Body.Close()
 
 	dat, err := io.ReadAll(resp.Body)
