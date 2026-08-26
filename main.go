a9c490824a8d798d53dc9c3ad936fc8419617df1
j3arch <j3archx@gmail.com>
2026-08-26 23:15:36 +0300

    main update

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
+		commands:      getCommands(),
+		caughtPokemon: map[string]pokeapi.Pokemon{},
 		pokeapiClient: pokeClient,
 	}
 
