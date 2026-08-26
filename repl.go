094d28097b7f8095ada211b043bcc29875ab51a8
j3arch <j3archx@gmail.com>
2026-08-26 23:34:42 +0300

    repl update for inspect

--- repl.go
+++ repl.go
@@ -77,6 +77,11 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught pokemon",
+			callback:    commandInspect,
+		},
 		"explore": {
 			name:        "explore <location_name>",
 			description: "Explore a location",
