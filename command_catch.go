5b1fc9321b4a0dfab7422f38e40e8310f7b48581
j3arch <j3archx@gmail.com>
2026-08-27 23:09:45 +0300

    empty input handling

--- command_catch.go
+++ command_catch.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"errors"
+)
+
 func commandCatch(cfg *config, args ...string) error {
-	return nil
+	if len(args) != 1 {
+		return errors.New("You must provide a pokemon name.")
+	}
 
 }
