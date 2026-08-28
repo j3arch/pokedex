38e876e292b4174a2d19327c9421c4c053e2a709
j3arch <j3archx@gmail.com>
2026-08-28 23:06:45 +0300

    get method

--- internal/pokeapi/pokemon_get.go
+++ internal/pokeapi/pokemon_get.go
@@ -1,7 +1,17 @@
 package pokeapi
 
-func (c *Client) GetPokemon(pokemonName string) (Pokemon, error) {
-	pokemonResp := Pokemon{}
+import "encoding/json"
 
-	return pokemonResp, nil
+func (c *Client) GetPokemon(pokemonName string) (Pokemon, error) {
+	url := baseURL + "/pokemon/" + pokemonName
+
+	if val, ok := c.cache.Get(url); ok {
+		pokemonResp := Pokemon{}
+		err := json.Unmarshal(val, &pokemonResp)
+		if err != nil {
+			return Pokemon{}, err
+		}
+		return pokemonResp, nil
+	}
+
 }
