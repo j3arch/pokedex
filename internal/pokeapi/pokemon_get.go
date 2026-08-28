fc3fc5f0919736564b1abb0309b107ccb542b56b
j3arch <j3archx@gmail.com>
2026-08-28 23:17:22 +0300

    data unpack

--- internal/pokeapi/pokemon_get.go
+++ internal/pokeapi/pokemon_get.go
@@ -2,6 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
@@ -18,6 +19,17 @@ func (c *Client) GetPokemon(pokemonName string) (Pokemon, error) {
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return Pokemon{}, err
 	}
