45b488cef747dcd0798d11d35c0a28ce3e263431
j3arch <j3archx@gmail.com>
2026-08-29 22:20:32 +0300

    pokedex callback

--- repl.go
+++ repl.go
@@ -97,6 +97,11 @@ func getCommands() map[string]cliCommand {
 			description: "Get the previous page of locations",
 			callback:    commandMapb,
 		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "See all the pokemon you have caught",
+			callback:    commandPokedex,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
