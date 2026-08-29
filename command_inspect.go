b4d0704d40e77f2f60abe84499047806cde1130d
j3arch <j3archx@gmail.com>
2026-08-29 22:14:24 +0300

    error handling

--- command_inspect.go
+++ command_inspect.go
@@ -1,6 +1,10 @@
 package main
 
+import "errors"
+
 func commandInspect(cfg *config, args ...string) error {
-	return nil
+	if len(args) != 1 {
+		return errors.New("You must proivde a pokemon name")
+	}
 
 }
